7af8046007723dffeecc9155ae99dcc8003a46e7
Bruno Morgado <brunomorgadowork@gmail.com>
2026-07-27 23:53:26 -0300

    refactor(clean): remove obsolete dead code and unused components

--- frontend/src/routes/_app/estabelecimentos.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/_app/estabelecimentos")({
-  beforeLoad: () => {
-    throw redirect({ to: "/leads-b2b" });
-  },
-});
